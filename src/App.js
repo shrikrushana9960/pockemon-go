c3573c3d1630a25c2557a876f44055c0b5135f81
shrikrushana <papupingale123@gmail.com>
2023-03-03 19:32:26 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,73 +1,83 @@
-import {  Divider, List, Skeleton } from "antd";
+import { Divider, List, Pagination, Skeleton, Spin } from "antd";
 import { useEffect, useState } from "react";
 import React from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-const PockemonCard = React.lazy(() => import('./components/PockemonCard'));
-const SearchBox = React.lazy(() => import('./components/SearchBox'));
+const PockemonCard = React.lazy(() => import("./components/PockemonCard"));
+const SearchBox = React.lazy(() => import("./components/SearchBox"));
 
 const App = () => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
   const [count, setCount] = useState();
   const [page, setPage] = useState(0);
-  const loadMoreData = () => {
+  const loadMoreData = (value) => {
     if (loading) {
       return;
     }
     console.log(count);
     setPage(page + 1);
     setLoading(true);
-    fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=${50 * page}`)
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=${50 * value}`)
       .then((res) => res.json())
       .then((body) => {
         setCount(body.count);
-        setData([...data, ...body.results]);
+        setData([...body.results]);
         setLoading(false);
       })
       .catch(() => {
         setLoading(false);
       });
   };
+  function onChnage(value) {
+    loadMoreData(value - 1);
+  }
   useEffect(() => {
-    loadMoreData();
+    loadMoreData(0);
   }, []);
   return (
     <div
       id="scrollableDiv"
       style={{
-        // height: 500,
-        overflow: "auto",
         padding: "0 16px",
         border: "1px solid rgba(140, 140, 140, 0.35)",
       }}
     >
-      <h1 style={{textAlign:"center"}}>Pockemon Go</h1>
-      <SearchBox/>
-      {count && (
-        <InfiniteScroll
-          dataLength={data.length}
-          next={loadMoreData}
-          hasMore={data.length < count}
-          loader={
-            <Skeleton
-              avatar
-              paragraph={{
-                rows: 1,
-              }}
-              active
-            />
-          }
-          endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
-          // scrollableTarget="scrollableDiv"
+      <h1 style={{ textAlign: "center" }}>Pockemon Go</h1>
+      <SearchBox />
+
+      {loading ? (
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            margin: "30px",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
         >
-          <List
-            grid={{ gutter: 16, column: 4 }}
-            dataSource={data}
-            renderItem={(item) => <PockemonCard item={item} />}
-          />
-        </InfiniteScroll>
+          {" "}
+          <Spin />{" "}
+        </div>
+      ) : (
+        <List
+          grid={{ gutter: 16, column: 3 }}
+          dataSource={data}
+          renderItem={(item) => <PockemonCard item={item} />}
+        />
       )}
+
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          margin: "30px",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Pagination total={940} hidden onChange={onChnage} showQuickJumper />
+      </div>
     </div>
   );
 };
